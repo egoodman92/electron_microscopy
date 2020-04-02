1ec9af83b443b9a2ae8860ee8d594d5646da9ec8
egoodman92 <55966728+egoodman92@users.noreply.github.com>
2020-04-02 08:35:02 -0700

    Delete NumberPictures.py

--- NumberPictures.py
+++
@@ -1,9 +0,0 @@
-import os
-path = 'models/images/'
-counter = 1
-for f in os.listdir(path):
-    suffix = f.split('.')[-1]
-    if suffix == 'jpg' or suffix == 'png':
-        new = '{}.{}'.format(str(counter), suffix)
-        os.rename(path + f, path + new)
-        counter = int(counter) + 1
